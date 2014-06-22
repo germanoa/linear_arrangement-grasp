73f4fb37c36cf39928aa939f3acbfa2215c3d461
Germano Andersson <germanoa@gmail.com>
2014-06-22 00:34:33 -0300

    grasp5 cleaned

--- src/la-grasp5.cpp
+++ src/la-grasp5.cpp
@@ -37,7 +37,6 @@ int my_random;
 int sol[MAXVERTEX];
 ofstream logfile;
 float exec_time;
-//hashtable com solucoes iniciais jah testadas
 
 void init_sol() {
         for (int i=0; i<MAXVERTEX;++i) {
@@ -46,19 +45,15 @@ void init_sol() {
 }
 
 void read_input() {
-	// n vertexes
 	cin >> n;
-	// m edges
 	cin >> m;
 
 	int temp;
-	//degrees.assign(n);
 	for (int i=0; i<n; ++i) {
 		cin >> temp;
 		degrees.push_back(temp);
 	}
 
-	//neighbors.assign(2*m);
 	for (int i=0; i<2*m; ++i) {
 		cin >> temp;
 		neighbors.push_back(temp);
@@ -67,7 +62,6 @@ void read_input() {
 	//-1
 	cin >> null;
 
-	//n_neighbors.assign(n+1);
 	for (int i=0; i<n+1; ++i) {
 		cin >> temp;
 		n_neighbors.push_back(temp);
@@ -92,7 +86,6 @@ int solution_cost() {
 	for (int u=0; u<n; ++u) {
 		for (int v=0; v < (int) al[u].size(); ++v) {
 			cost +=abs(sol[u] - sol[al[u][v]]);
-			//cout << u << "-" << al[u][v] << ": "<< cost << endl;
 		}
 	}
 
@@ -106,7 +99,7 @@ int solution_cost() {
 	return cost;
 }
 
-//move
+//move: elementary modification
 void static inline _invert(int i, int j) {
 	int temp = sol[i];
 	sol[i] = sol[j];
@@ -121,22 +114,15 @@ void print_solution() {
 
 static inline int _diff_cost(int v1, int v2, int bigger, int _it, int _over) {
 	int ret=0;
-	//cout << "_it: " << _it << endl;
-	//cout <<"vizinhos de:" << endl;
 	for (int v=0; v < _it; ++v) {
-		//cout << v1 << ": " << al[v1][v] << endl;
 		ret +=abs(sol[v1] - sol[al[v1][v]]);
 		ret +=abs(sol[v2] - sol[al[v2][v]]);
 
-		//cout << v2 << ": " << al[v2][v] << endl;
 	}
-	//cout << "__it+_over: " << _it+_over << endl;
 	for (int v=_it; v < _it+_over; ++v) {
 		if (bigger==1) {
-			//cout << v1 << ": " << al[v1][v] << endl;
 			ret +=abs(sol[v1] - sol[al[v1][v]]);
 		} else {
-			//cout << v2 << ": " << al[v2][v] << endl;
 			ret +=abs(sol[v2] - sol[al[v2][v]]);
 		}
 	}
@@ -146,8 +132,6 @@ static inline int _diff_cost(int v1, int v2, int bigger, int _it, int _over) {
 void update_cost(int v1, int v2) {
 	int v1_neighbors, v2_neighbors;
 
-	//cout << "v1: " << v1 << " v2: " << v2 << endl;
-	
 	//to save neighbors iterations
 	v1_neighbors = (int) al[v1].size();
 	v2_neighbors = (int) al[v2].size();
@@ -163,28 +147,13 @@ void update_cost(int v1, int v2) {
 	}
 
 	int diff_to_del = _diff_cost(v1,v2,bigger,_it,_over);
-	//cout <<"diff_to_del: " << diff_to_del << endl;
-
-	//if (my_random) {
-	//	v2 = rand() % n; 
-	//	_invert(v1,v2);
-
-	//} else {
-	//	_invert(v1,v2);
-	//}
 	_invert(v1,v2);
-
 	int diff_to_add = _diff_cost(v1,v2,bigger,_it,_over);
-	//cout <<"diff_to_add: " << diff_to_add << endl;
 
 
-	//x = x - diff_to_del;
-	//x += diff_to_add;
 	if (diff_to_del>diff_to_add) {
 		x = x - diff_to_del;
 		x += diff_to_add;
-		//cout <<"new x: " << x << endl;
-	//print_solution();
 	} else {
 		_invert(v2,v1); //revert attempt
 	}
@@ -194,9 +163,6 @@ void update_cost(int v1, int v2) {
 
 
 void print_status() {
-	//logfile.open("out/logfile.txt",ios::app);
-	//logfile << infile << ";" << first_x << ";" << best_x << ";" << setprecision(6) << exec_time << ";" << it << ";" << endl;
-	//logfile.close();
 	cout << infile << ";" << first_x << ";" << best_x << ";" << setprecision(6) << exec_time << ";" << it << ";" << tentativas << endl;
 }
 
@@ -224,8 +190,6 @@ void construct() {
                 sol[selected]=order++;
 
                 //greedy part
-		//for (it_al = al[selected].begin(); it_al != al[selected].end(); ++it_al) {
-                //        neighbor = *it_al;
 		for (int v=0; v < (int) al[selected].size(); ++v) {
                         neighbor = al[selected][v];
                         if (sol[neighbor] == FREE) {
@@ -273,8 +237,6 @@ void local_search() {
 	for (int i=0; i<partitions-1; ++i) {
 		for (int j=0; j<THREADS; ++j) { 
 			update_cost(j+i,j+i+1);
-			//_invert(j+i,j+i+1);
-			//temp = solution_cost();
 		}
 	}
 }
@@ -285,7 +247,7 @@ int parse_args(int argc, char* argv[]) {
 		return 0;
 	}
 
-	it = atoi(argv[1]); //iterations
+	it = atoi(argv[1]);
 	my_random = atoi(argv[2]);
 	infile = argv[3];
 	return 1;
@@ -306,7 +268,6 @@ int main(int argc, char* argv[]) {
 	//GRASP
 	for (int i=0; i<it; ++i) {
 		construct();
-		//print_solution();
 		if (is_new_initial_solution()) {
 			local_search();
 			if (x < best_x) {
